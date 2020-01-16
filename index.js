1e0c82e254e73b6989bb682cc64c4e595157c34d
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-01-17 00:45:42 +0100

    starting over

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-var mongoose = require("mongoose");
-
-const winesRoute = require("./src/routes/winesRoute");
 
 const port = process.env.PORT;
-
+var mongoose = require("mongoose");
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -27,7 +24,6 @@ mongoose
   );
 
 app.get("/", (req, res) => res.send("Server is up and running!"));
-app.use("/wines", winesRoute);
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}!`);
 });
--- src/schema/winesSchema.js
+++
@@ -1,4 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var WineSchema = new Schema({}, { collection: "wines" });
-module.exports = mongoose.model("wines", WineSchema);

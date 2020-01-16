d4f629d19bc954258571d097ac573866522f1e23
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-01-16 11:52:41 +0100

    removed await from mongoose.connection

--- src/wines.js
+++ src/wines.js
@@ -17,7 +17,7 @@ router.get("/", async (req, res) => {
     });
 
     //Get the default connection
-    const db = await mongoose.connection;
+    const db =  mongoose.connection;
 
     //Bind connection to error event (to get notification of connection errors)
     db.on("error", console.error.bind(console, "MongoDB connection error:"));

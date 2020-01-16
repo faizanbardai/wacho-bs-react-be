61f1b025abeeb73a91760803a221c48a57f8a9eb
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-01-16 11:37:35 +0100

    moved db connection inside try-catch

--- src/wines.js
+++ src/wines.js
@@ -6,30 +6,30 @@ var mongoose = require("mongoose");
 
 require("dotenv").config();
 
-//Set up default mongoose connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
-
-//Get the default connection
-const db = mongoose.connection;
-
-//Bind connection to error event (to get notification of connection errors)
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
-
-// Define schema
-var Schema = mongoose.Schema;
-
-var WineSchema = new Schema({}, { collection: "wines" });
-
-// Compile model from schema
-const Wines = mongoose.model("wines", WineSchema);
-
 router.get("/", async (req, res) => {
   try {
+    //Set up default mongoose connection
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+      useUnifiedTopology: true
+    });
+
+    //Get the default connection
+    const db = await mongoose.connection;
+
+    //Bind connection to error event (to get notification of connection errors)
+    db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+    // Define schema
+    var Schema = mongoose.Schema;
+
+    var WineSchema = new Schema({}, { collection: "wines" });
+
+    // Compile model from schema
+    const Wines = mongoose.model("wines", WineSchema);
+
     const response = await Wines.find();
     res.send(response);
   } catch (error) {

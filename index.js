2b3e9ed7aeae3e2b26eee08d36e7660c31b3d5d5
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-01-16 17:39:38 +0100

    fix: another try

--- index.js
+++ index.js
@@ -7,19 +7,24 @@ const wines = require("./src/routes/wines");
 
 const port = process.env.PORT;
 
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  })
+  .then(
+    () => {
+      console.log("Connected!");
+    },
+    err => {
+      console.log(err);
+    }
+  );
+
 app.get("/", (req, res) => res.send("Server is up and running!"));
 app.use("/wines", wines);
-app.listen(port, async () => {
-  try {
-    const connectionResult = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-      useUnifiedTopology: true
-    });
-    console.log("Connected!");
-  } catch (error) {
-    console.log(error);
-  }
+app.listen(port, () => {
   console.log(`Server is listening on port ${port}!`);
 });
--- src/schema/wines.js
+++ src/schema/wines.js
@@ -1,7 +1,4 @@
 var mongoose = require('mongoose');
-
 var Schema = mongoose.Schema;
-
 var WineSchema = new Schema({}, { collection: "wines" });
-
 module.exports = mongoose.model("wines", WineSchema);

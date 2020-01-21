d62b0767aadceb5ddb316db8fb3f2148258c3195
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-01-21 23:28:14 +0100

    whitelisted github.io live link

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => res.send("Server is up and running!"));
 
 app.use(bodyParser.json());
 
-var whitelist = ["http://localhost:3000/admin", "http://localhost:3000"];
+var whitelist = ["http://localhost:3000", "https://faizanbardai.github.io"];
 var corsOptions = {
   origin: function(origin, callback) {
     if (whitelist.indexOf(origin) !== -1 || !origin) {

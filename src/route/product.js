a0bf6085cb1e302e9b66937bd34bfb0b9686abc0
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-04-29 12:50:04 +0200

    Active product, Update product

--- src/route/product.js
+++ src/route/product.js
@@ -1,23 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 const productModel = require("../model/product");
 
-router.post("/", async (req, res) => {
+router.post("/", passport.authenticate("jwt"), async (req, res) => {
   const newWine = await productModel.create(req.body);
   res.send(newWine);
 });
 
 router.get("/", async (req, res) => {
-  res.send(await productModel.find({ active: true }));
+  res.send(
+    await productModel.find(req.query.active == 1 ? { active: true } : null)
+  );
 });
 
-router.put("/", async (req, res) => {
-  const { id, inventory } = req.body;
+router.put("/", passport.authenticate("jwt"), async (req, res) => {
+  const { _id } = req.body;
+
   try {
     const response = await productModel.findByIdAndUpdate(
-      id,
-      { inventory: inventory },
-      { new: true }
+      _id,
+      { ...req.body },
+      {
+        new: true,
+      }
     );
     res.json(response);
   } catch (error) {

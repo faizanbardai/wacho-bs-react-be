fa59d0e3d6c687a538d50f257a467fd87dd32a41
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-08-07 01:18:11 +0200

    new build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.30b3deda76ae2e8b8198acdf7f63a9ba.js"
+  "./precache-manifest.ee80c2d89056088daaa2d453fa0ca609.js"
 );
 
 self.addEventListener('message', (event) => {
